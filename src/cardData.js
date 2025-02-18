186244544a125126c8228cfcb25a3f37763682c8
Maxbouvierpro <maxbouvier.pro@gmail.com>
2025-02-18 21:25:33 +0800

    add /src

--- src/cardData.js
+++ src/cardData.js
@@ -4,42 +4,42 @@ const dataCards = [
     {
         id:1,
         name:'Shoguntoto',
-        imgsrc:'/assets/image.png',
+        imgsrc:'src/assets/image.png',
         comment:`C'est une belle plateforme, les explications sont clairs, les maquettes professionnelles.`,
         at:'@thomasdev59'
     },
     {
         id:2,
         name:'Shoguntoto',
-        imgsrc:'/assets/image-1.png',
+        imgsrc:'src/assets/image-1.png',
         comment:`Ezis répond à toutes les questions afin de nous aider le plus possible dans notre objectif.`,
         at:'@thomasdev59'
     },
     {
         id:3,
         name:'Hocine',
-        imgsrc:'/assets/image-2.png',
+        imgsrc:'src/assets/image-2.png',
         comment:`J'aime bien le concept d'avoir une communauté, Ezis est très à l'écoute et n'hésite pas à répondre aux questions.`,
         at:'@hocine'
     },
     {
         id:4,
         name:'Youtmax654',
-        imgsrc:'/assets/image-3.png',
+        imgsrc:'src/assets/image-3.png',
         comment:`Je me suis grandement améliorer sur les technologies que j'utilise et sur ma façon de traviller sur un projet.`,
         at:'@youtmax654'
     },
     {
         id:5,
         name:'Baptiste',
-        imgsrc:'/assets/image-4.png',
+        imgsrc:'src/assets/image-4.png',
         comment:`Des projets de qualité avec toutes les ressources dont on aurait besoin pour les réaliser.`,
         at:'@baptiste_lechat'
     },
     {
         id:6,
         name:'Le D',
-        imgsrc:'/assets/image-5.png',
+        imgsrc:'src/assets/image-5.png',
         comment:`Le fait d'avoir accès à un discord et une communauté fait vraiment la différence.`,
         at:'@sport_devweb'
     }
